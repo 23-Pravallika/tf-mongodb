eecec42d2ba1e9f87490d7b2352e63f40ffa96d1
23-Pravallika <learngain23@gmail.com>
2024-02-16 21:42:06 +0530

    AutoPush

--- docdb.tf
+++ docdb.tf
@@ -8,6 +8,7 @@ resource "aws_docdb_cluster" "docdb" {
 #   preferred_backup_window = "07:00-09:00"
   skip_final_snapshot     = true
   db_subnet_group_name    = aws_docdb_subnet_group.docdb_subnet_group.name
+  vpc_security_group_ids  = [aws_security_group.allow-docdb.id]
 }
 
 # Creates Subnet Group Needed to host the docdb cluster 
@@ -20,4 +21,11 @@ resource "aws_docdb_subnet_group" "docdb_subnet_group" {
   }
 }
 
+# Creates Instances Needed for the DocDB Cluster
+resource "aws_docdb_cluster_instance" "cluster_instances" {
+  count              = 1
+  identifier         = "robo-${var.ENV}-docdb-cluster-instance"
+  cluster_identifier = aws_docdb_cluster.docdb.id
+  instance_class     = "db.t3.medium"
+}
 
