eccac56beb4b2c9af2700ad1d67cb7c175961bf7
23-Pravallika <learngain23@gmail.com>
2024-02-16 17:58:28 +0530

    AutoPush

---
+++ docdb.tf
@@ -0,0 +1,21 @@
+#creates the cluster
+resource "aws_docdb_cluster" "docdb" {
+  cluster_identifier      = "my-docdb-cluster"
+  engine                  = "docdb"
+  master_username         = "admin1"
+  master_password         = "roboshop1"
+#   backup_retention_period = 5
+#   preferred_backup_window = "07:00-09:00"
+  skip_final_snapshot     = true
+}
+
+# Creates Subnet Group Needed to host the docdb cluster 
+resource "aws_docdb_subnet_group" "docdb_subnet_group" {
+  name       = "robo-${var.ENV}-docdb-subnet-group"
+  subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
+
+  tags = {
+    Name = "robo-${var.ENV}-docdb-subnet-group"
+  }
+}
+
