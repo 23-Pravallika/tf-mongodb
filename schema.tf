6cc715ec2036b78ff4f8191981cbe4c7b8db1009
23-Pravallika <learngain23@gmail.com>
2024-02-18 16:25:49 +0530

    AutoPush

--- schema.tf
+++ schema.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "schema" {
         
-    depends_on = [aws_docdb_cluster_instance.cluster_instances]  
+    depends_on = [aws_docdb_cluster.docdb, aws_docdb_cluster_instance.cluster_instances]  
   
   provisioner "local-exec" {
     command = <<EOF
@@ -8,9 +8,9 @@ resource "null_resource" "schema" {
     curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
     unzip mongodb.zip
     cd mongodb-main
-    wget https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem 
-    mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} --sslCAFile rds-combined-ca-bundle.pem --username admin1 --password roboshop1 < catalogue.js
-    mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} --sslCAFile rds-combined-ca-bundle.pem --username admin1 --password roboshop1 < users.js
+    wget https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem 
+    mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} --sslCAFile global-bundle.pem --username admin1 --password roboshop1 < catalogue.js
+    mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} --sslCAFile global-bundle.pem --username admin1 --password roboshop1 < users.js
     EOF
   }
 }
