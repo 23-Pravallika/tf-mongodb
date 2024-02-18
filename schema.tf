2c018e5f2a6af8baf092f1c1054eaca841328214
23-Pravallika <learngain23@gmail.com>
2024-02-18 16:43:46 +0530

    AutoPush

--- schema.tf
+++ schema.tf
@@ -1,17 +1,17 @@
-resource "null_resource" "schema" {
+# resource "null_resource" "schema" {
         
-    depends_on = [aws_docdb_cluster.docdb, aws_docdb_cluster_instance.cluster_instances]  
+#     depends_on = [aws_docdb_cluster.docdb, aws_docdb_cluster_instance.cluster_instances]  
   
-  provisioner "local-exec" {
-    command = <<EOF
-    cd /tmp
-    curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
-    unzip mongodb.zip
-    cd mongodb-main
-    wget https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem 
-    mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} --sslCAFile global-bundle.pem --username admin1 --password roboshop1 < catalogue.js
-    mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} --sslCAFile global-bundle.pem --username admin1 --password roboshop1 < users.js
-    EOF
-  }
-}
+#   provisioner "local-exec" {
+#     command = <<EOF
+#     cd /tmp
+#     curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
+#     unzip mongodb.zip
+#     cd mongodb-main
+#     wget https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem 
+#     mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} --sslCAFile global-bundle.pem --username admin1 --password roboshop1 < catalogue.js
+#     mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} --sslCAFile global-bundle.pem --username admin1 --password roboshop1 < users.js
+#     EOF
+#   }
+# }
 
