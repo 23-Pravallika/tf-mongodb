c311b389f001fd2968f19ad1072ed455593d6692
23-Pravallika <learngain23@gmail.com>
2024-02-19 14:53:00 +0530

    AutoPush

---
+++ locals.tf
@@ -0,0 +1,9 @@
+# Whenever you have some common fields or anything which is highly used, you can consider that in locals and can call it on your need
+
+locals {
+    DOCDB_USER = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["DOCDB_USR"]
+    DOCDB_PASSWD = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["DOCDB_PASS"]
+}
+
+
+
